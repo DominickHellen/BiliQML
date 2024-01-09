5db9f1bfa5126f8e1d6a48cdbf28224d0c1683ef
D Hellen <88243822+DominickHellen@users.noreply.github.com>
2024-01-09 17:19:00 -0500

    Update and rename Macrophage detection script.groovy to BiliQML_Detection.groovy

--- BiliQML_Detection.groovy
+++ BiliQML_Detection.groovy
@@ -1,4 +1,4 @@
-//This` .Groovy script is for automated batch detection of macrophages within liver sections.//
+//This .Groovy script is for automated batch detection of cholangiocyte cell groupings within liver sections.//
 
 //This line of code is placed before and after analyzing each slide//
 //It is used to pause the current thread for 100 milliseconds//
@@ -15,12 +15,3 @@ selectAnnotations();
 
 //Creates objects within each annotation based on the threshold set for 'Macrophages' classifier//
 createDetectionsFromPixelClassifier("ss", 30.0, 0.0, "SPLIT", "DELETE_EXISTING")
-
-//Clears RAM and thread after each slide//
-Thread.sleep(100)
-javafx.application.Platform.runLater {
-getCurrentViewer().getImageRegionStore().cache.clear()
-System.gc()
-}
-Thread.sleep(100)
-
